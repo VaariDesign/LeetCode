c446a2d0494d86b3b16e9f6b807e098626fc28e2
VaariDesign <84449958+VaariDesign@users.noreply.github.com>
2022-10-18 09:12:20 +0200

    Create 0001_two_sums.py

---
+++ 0001_two_sums.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       nums_d = {};
+       for i, n in enumerate(nums):
+           if((target - n) in nums_dict.keys()):
+               return [nums_dict[target - n], i]
+           nums_d[n] = i
