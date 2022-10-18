ef126efd71dcdeb4594997e7cc7b1bad0e877c69
VaariDesign <84449958+VaariDesign@users.noreply.github.com>
2022-10-18 08:46:52 +0200

    Create 38_count_and_say.py

---
+++ 38_count_and_say.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countAndSay(self, n):
+        sequence = "1"
+        for i in range(n - 1):
+            sequence = self.getNext(sequence)
+        return sequence
+
+    def getNext(self, sequence):
+        i, next_sequence = 0, ""
+        while i < len(sequence):
+            count = 1
+            while i < len(sequence) - 1 and sequence[i] == sequence[i + 1]:
+                count += 1
+                i += 1
+            next_sequence += str(count) + sequence[i]
+            i += 1
+        return next_sequence
+    
+if __name__ == "__main__":
+    for i in range(1, 4):
+        print(Solution().countAndSay(i))
